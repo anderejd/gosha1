0a1cbc4d8e625bafce074a254cb3d976b9599028
Anders Rejdebrant <rajder@gmail.com>
2015-09-27 09:26:31 +0200

    Walking subdirs.

--- main.go
+++ main.go
@@ -3,19 +3,52 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
+	"path/filepath"
 )
+
+func ProcessFile(path string) (error) {
+	return nil
+}
+
+func ProcessDir(path string) (error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	list, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		return err
+	}
+	for _, f := range list {
+		p := filepath.Join(path, f.Name())
+		if !f.IsDir() {
+			err = ProcessFile(p)
+			if nil != err {
+				return err
+			}
+			continue
+		}
+		err = ProcessDir(p)
+		if nil != err {
+			return err
+		}
+		fmt.Println(p)
+	}
+	return nil
+}
 
 func main() {
 	flag.Parse()
-	path := flag.Arg(0)
-	if "" == path {
-		fmt.Fprintln(os.Stderr, "Arg 0 (path) missing.")
+	dirpath := flag.Arg(0)
+	if "" == dirpath {
+		fmt.Fprintln(os.Stderr, "Arg 0 (dirpath) missing.")
 		os.Exit(1)
 	}
-	files, _ := ioutil.ReadDir(path)
-	for _, f := range files {
-		fmt.Println(f.Name())
+	err := ProcessDir(dirpath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error: ", err)
+		os.Exit(1)
 	}
 }
