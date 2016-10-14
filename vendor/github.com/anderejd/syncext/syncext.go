8455010fa8f8b80853dc8380a88ca1aaa15075d9
Anders <rajder@gmail.com>
2016-10-14 21:37:26 +0200

    Added blocking option for FanOut.

--- vendor/github.com/anderejd/syncext/syncext.go
+++ vendor/github.com/anderejd/syncext/syncext.go
@@ -6,11 +6,14 @@ package syncext
 import "sync"
 
 /**
- * Starts a group of goroutines and calls done when all of them are done.
+ * Starts and waits for a group of goroutines. This function is blocking if the
+ * callback is nil.
  *
  * @param numWorkers  Number of goroutines to start.
  * @param work        The function to start as goroutine(s).
- * @param done        The function to call when all workers are done.
+ * @param done        The function to call when all workers are done. If this is
+ *                    nil this function will block until all workers have
+ *                    finished.
  */
 func FanOut(numWorkers int, work, done func()) {
 	var wg sync.WaitGroup
@@ -20,6 +23,10 @@ func FanOut(numWorkers int, work, done func()) {
 			work()
 			wg.Done()
 		}()
+	}
+	if nil == done {
+		wg.Wait()
+		return
 	}
 	go func() {
 		wg.Wait()
